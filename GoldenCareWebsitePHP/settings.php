35081c2991a636ca5b00cdf5bb7d9957bac21ca4
Valentino7910 <141201110+Valentino7910@users.noreply.github.com>
2024-05-05 05:23:18 +1000

    Delete GoldenCareWebsitePHP directory

--- GoldenCareWebsitePHP/settings.php
+++
@@ -1,7 +0,0 @@
-<?php
-$host = "localhost";
-$user = "root"; // your user name
-$pswd = "root"; // your password d(date of birth – ddmmyy)
-$dbnm = "golden_care"; // your database
-$conn = new mysqli($host, $user, $pswd, $dbnm);
-?>
