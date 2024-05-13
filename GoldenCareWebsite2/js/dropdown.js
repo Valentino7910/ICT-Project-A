ee67988514a4e99d9805ae7af837666d304b6e4e
Valentino7910 <141201110+Valentino7910@users.noreply.github.com>
2024-05-13 19:01:14 +1000

    Delete GoldenCareWebsite2 directory

--- GoldenCareWebsite2/js/dropdown.js
+++
@@ -1,4 +0,0 @@
-function toggleDropdown() {
-    var dropdown = document.getElementById("userDropdown");
-    dropdown.style.display = dropdown.style.display === 'block' ? 'none' : 'block';
-}
--- GoldenCareWebsite2/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-// Unset username session variable
-unset($_SESSION['username']);
-// Optionally destroy the entire session if not used elsewhere
-session_destroy();
-header("Location: index.php"); // Redirect to the homepage after logout
-exit();
-?>
